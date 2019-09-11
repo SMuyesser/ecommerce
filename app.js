7982b0323b11026b6d9fdbb470670114e00dfcd1
SMuyesser <smuyesser@gmail.com>
2019-09-10 18:09:55 -0700

    DB created and connected

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const app = express();
-
+const mongoose = require('mongoose');
 //allows use to use .env file
 require('dotenv').config();
 
+// app
+const app = express();
+
+// database
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+}).then(() => console.log('DB connected'))
+
+// routes
 app.get('', (req, res) =>  {
-	res.send("hello from node");
+	res.send("Welcome To Your Homepage!");
 });
 
 //gives us access to env file and the port
