1170bb3b65030e020f737135d5e9930e1124b8a7
SMuyesser <smuyesser@gmail.com>
2019-09-12 15:49:46 -0700

    product model created

---
+++ models/product.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+// Use the id to find category from category schema
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema({
+		name: {
+			type: String,
+			trim: true,
+			required: true,
+			maxlength: 32
+		},
+		description: {
+			type: String,
+			trim: true,
+			required: true,
+			maxlength: 2000
+		},
+		price: {
+			type: Number,
+			trim: true,
+			required: true,
+			maxlength: 32
+		},
+		category: {
+			// get type from category model refers to category model
+			type: ObjectId,
+			ref: 'Category',
+			required: true
+		},
+		quantity: {
+			type: Number
+		},
+		photo: {
+			data: Buffer,
+			contentType: String
+		},
+		shipping: {
+			required: false,
+			type: Boolean
+		}
+	}, 
+	{timestamps: true} 
+);
+
+module.exports = mongoose.model("Product", productSchema);
