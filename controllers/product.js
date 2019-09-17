c56f7f2aa1270ec0698c627887a3d5f5989a3621
SMuyesser <smuyesser@gmail.com>
2019-09-17 14:39:35 -0700

    Create product update method

--- controllers/product.js
+++ controllers/product.js
@@ -28,7 +28,7 @@ exports.read = (req, res) => {
 };
 
 exports.create = (req, res) => {
-	// create new form from incoming form data
+	// get incoming form data
 	let form = new formidable.IncomingForm();
 	form.keepExtensions = true;
 	// if errors, respond with errors
@@ -67,6 +67,55 @@ exports.create = (req, res) => {
 			}
 			// if no error, our product saved successfully
 			res.json(result);
+		});
+	});
+};
+
+exports.update = (req, res) => {
+	// get incoming form data
+	let form = new formidable.IncomingForm();
+	form.keepExtensions = true;
+	// if errors, respond with errors
+	form.parse(req, (err, fields, files) => {
+		if(err) {
+			res.status(400).json({
+				error: "Image could not be uploaded"
+			});
+		}
+		// check for all fields
+		const { name, description, price, category, quantity, shipping } = fields;
+		if(!name || !description || !price || !category || !quantity || !shipping) {
+			return res.status(400).json({
+				error: "All fields are required"
+			});
+		}
+		// with no errors, update product using data in fields
+		let product = req.product;
+		// extend is from lodash
+		product = _.extend(product, fields);
+		// if were are sending an image file from front end
+		if(files.photo) {
+			if(files.photo.size > 1000000) {
+				return res.status(400).json({
+					error: "Image should be less than 1mb in size"
+			});
+			}
+			// photo data = file system, read files syncronously at the photo path
+			product.photo.data = fs.readFileSync(files.photo.path);
+			product.photo.contentType = files.photo.type;
+		}
+		// save our product if no errors
+		product.save((err, result) => {
+			if(err) {
+				return res.status(400).json({
+					error: errorHandler(err)
+				});
+			}
+			// if no error, our product saved successfully
+			res.json({
+				result,
+				message: "Product updated successfully"
+			});
 		});
 	});
 };
--- routes/product.js
+++ routes/product.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read, remove } = require('../controllers/product');
+const { create, productById, read, remove, update } = require('../controllers/product');
 const { requireSignIn, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
 router.get('/product/:productId', read);
 router.post('/product/create/:userId', requireSignIn, isAuth, isAdmin, create);
-router.delete('/product/:productId/:userId', requireSignIn, isAuth, isAdmin, remove)
+router.delete('/product/:productId/:userId', requireSignIn, isAuth, isAdmin, remove);
+router.put('/product/:productId/:userId', requireSignIn, isAuth, isAdmin, update);
 
 // anytime there is userId in parameter, this method fires
 router.param('userId', userById);
