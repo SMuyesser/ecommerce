67984a92aa6a2fa3e6914c77c6209e736762711a
SMuyesser <smuyesser@gmail.com>
2019-09-17 14:18:19 -0700

    Create product remove method

--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read } = require('../controllers/product');
+const { create, productById, read, remove } = require('../controllers/product');
 const { requireSignIn, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
 router.get('/product/:productId', read);
 router.post('/product/create/:userId', requireSignIn, isAuth, isAdmin, create);
+router.delete('/product/:productId/:userId', requireSignIn, isAuth, isAdmin, remove)
 
 // anytime there is userId in parameter, this method fires
 router.param('userId', userById);
