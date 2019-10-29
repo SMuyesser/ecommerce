20721ea3de539d7f29589636ca1a285778f385bb
SMuyesser <smuyesser@gmail.com>
2019-10-29 11:33:14 -0700

    adds get profile and update profile methods

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { requireSignIn, isAuth, isAdmin } = require('../controllers/auth');
-const { userById } = require('../controllers/user');
+const { userById, read, update } = require('../controllers/user');
 
 // test route requiring authentication, current authenticated user, and admin
 router.get('/secret/:userId', requireSignIn, isAuth, (req, res) => {
@@ -10,6 +10,10 @@ router.get('/secret/:userId', requireSignIn, isAuth, (req, res) => {
 		user: req.profile
 	});
 });
+
+router.get('/user/:userId', requireSignIn, isAuth, read);
+router.put('/user/:userId', requireSignIn, isAuth, update);
+
 
 // method takes two arguments. route and method from controller
 router.param('userId', userById);
