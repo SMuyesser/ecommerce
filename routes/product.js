5dffffa023b64dad9fed722723df2fd699278cc9
SMuyesser <smuyesser@gmail.com>
2019-10-29 11:21:04 -0700

    adds photo method

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read, remove, update, list, listRelated, listCategories, listBySearch } = require('../controllers/product');
+const { create, productById, read, remove, update, list, listRelated, listCategories, listBySearch, photo } = require('../controllers/product');
 const { requireSignIn, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
@@ -10,6 +10,7 @@ router.post('/product/create/:userId', requireSignIn, isAuth, isAdmin, create);
 router.delete('/product/:productId/:userId', requireSignIn, isAuth, isAdmin, remove);
 router.put('/product/:productId/:userId', requireSignIn, isAuth, isAdmin, update);
 router.post('/products/by/search', listBySearch);
+router.get('/product/photo/:productId', photo);
 router.get('/products', list);
 router.get('/products/categories', listCategories);
 router.get('/products/related/:productId', listRelated);
